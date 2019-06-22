06d2b7349fa6a5fbbe20c8e4881eadb723b3f101
Ben Osterman <benosterman@gmail.com>
2019-06-22 15:38:52 -0700

    convert to MVC design pattern

---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,16 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Ben Osterman on 6/22/19.
+//  Copyright © 2019 Ben Osterman. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
+
